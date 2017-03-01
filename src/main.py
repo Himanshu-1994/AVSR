7286cae62fc196cf22483b313bb0ef7934d1faf1
Himanshu <you@example.com>
2017-03-02 02:53:07 +0530

    clean

--- src/main.py
+++ src/main.py
@@ -25,13 +25,13 @@ import keras.callbacks
 
 # Global variables
 # grid_corpus = '../../../himanshu/grid_corpus/'
-grid_corpus = 'grid/'
+grid_corpus = '../'
 F = 50000.0
 
 # Input Parameters
-time_length = 60
+time_length = 120
 feature_length = 120
-num_class = 27
+num_class = 28
 max_string_len = 16
 
 # Something about datasets
@@ -57,16 +57,18 @@ if K.image_dim_ordering() == 'th':
 else:
     input_shape = (time_length, feature_length, 1)
 
-input_time_length = time_length // (pool_size ** 2) -2
+input_time_length = time_length // (pool_size ** 2)
 
 
 def text_to_labels(text):
     ret = []
+    ret.append(27)
+    ret.append(27)
     for char in text:
         if char >= 'a' and char <= 'z':
-            ret.append(ord(char) - ord('a'))
-        elif char == ' ':
-            ret.append(26)
+            ret.append(ord(char) - ord('a')+1)
+            ret.append(27)
+    ret.append(27)
     return ret
 
 def get_feature(audio):
@@ -81,7 +83,7 @@ def get_audio_label( size =100, cookies =[0,0,0]):
     [i0,j0,k0] = cookies
     count = 0
     data_audio , data_label = [],[]
-    for i in range(i0, 33): 
+    for i in range(i0, 32): 
 
         audio_list = np.sort(glob.glob(grid_corpus + 's' + str(i+1) + '/*.wav'))
         align_list = np.sort(glob.glob(grid_corpus + 's' + str(i+1) + '/align/*.align'))
@@ -123,34 +125,45 @@ def get_audio_label( size =100, cookies =[0,0,0]):
 
     return np.array(data_audio), np.array(data_label), cookies
 
-def getdata(time_length = 60, input_time_length = 13, max_string_len = 12, size =100, cookies =[0,0,0]):
-    data_audio, data_label, cookies = get_audio_label( size, cookies)
-    trainX, trainY, label_length = [], [], []
-    for i in range(size):
-        trainX.append(get_feature(data_audio[i]))
-        trainY.append(text_to_labels(data_label[i]))
-        label_length.append(len(data_label[i]))
-    trainX = sequence.pad_sequences(np.array(trainX), maxlen=time_length, padding='post', dtype=float)
-    trainY = sequence.pad_sequences(np.array(trainY), maxlen=max_string_len, padding='post', dtype=float)
-    input_length = np.ones(size)*input_time_length
-    label_length = np.array(label_length)
-    textX = data_label
+def getdata(time_length = 120, input_time_length = 30, max_string_len = 16, size =100, cookies =[0,0,0]):
+    while 1:
+        data_audio, data_label, cookies = get_audio_label( size, cookies)
+        trainX, trainY, label_length = [], [], []
+        for i in range(size):
+            trainX.append(get_feature(data_audio[i]))
+            trainY.append(text_to_labels(data_label[i]))
+            label_length.append(len(data_label[i]))
+        input_length = np.array([np.ceil(k.shape[0]/4.0) for k in trainX])
+        trainX = sequence.pad_sequences(np.array(trainX), maxlen=time_length, padding='post', dtype=float).reshape(-1,time_length,feature_length,1)
+        trainY = sequence.pad_sequences(np.array(trainY), maxlen=max_string_len, padding='post', dtype=float)
+        # input_length = np.ones(size)*input_time_length
+        label_length = np.array(label_length)
+        textY = data_label
 
-    return trainX, trainY, input_length, label_length, textX#, cookies
+        inputs = {'the_input': trainX,
+                  'the_labels': trainY,
+                  'input_length': input_length,
+                  'label_length': label_length,
+                  'source_str': textY  # used for visualization only
+                  }
+        outputs = {'ctc': np.zeros([size])}  # dummy data for dummy loss function
+        yield (inputs, outputs)
 
-def decode_batch(test_func, word_batch):
+    # return trainX, trainY, input_length, label_length, textX#, cookies
+
+def decode_batch(test_func, word_batch, input_length):
     out = test_func([word_batch])[0]
     ret = []
     for j in range(out.shape[0]):
-        out_best = list(np.argmax(out[j, 2:], 1))
+        out_best = list(np.argmax(out[j, 0:input_length[j]], 1))
         out_best = [k for k, g in itertools.groupby(out_best)]
-        # 26 is space, 27 is CTC blank char
+        # 27 is CTC blank char
         outstr = ''
         for c in out_best:
-            if c >= 0 and c < 26:
-                outstr += chr(c + ord('a'))
-            elif c == 26:
-                outstr += ' '
+            if c >= 1 and c < 27:
+                outstr += chr(c + ord('a')-1)
+            # elif c == 26:
+            #     outstr += ' '
         ret.append(outstr)
     return ret
 
@@ -158,58 +171,101 @@ def ctc_lambda_func(args):
     y_pred, labels, input_length, label_length = args
     # the 2 is critical here since the first couple outputs of the RNN
     # tend to be garbage:
-    y_pred = y_pred[:, 2:, :]   # The first argument should be from 2: , Since K.image_dim_ordering() == 'tf' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    y_pred = y_pred[:, :, :]   # The first argument should be from 2: , Since K.image_dim_ordering() == 'tf' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
-def build_model():
-    act = 'relu'
-    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
-    inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                          activation=act, init='he_normal', name='conv1')(input_data)
-    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
-    inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
-                          activation=act, init='he_normal', name='conv2')(inner)
-    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
-    conv_to_rnn_dims = (time_length // (pool_size ** 2), (feature_length // (pool_size ** 2)) * conv_num_filters)
-    inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
+class VizCallback(keras.callbacks.Callback):
 
-    # cuts down input size going into RNN: 
-    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)    # Giving dimension error ??????????????????????????????????????????????????
+    def __init__(self, test_func,text_img_gen,num_display_words=6):
+        self.test_func = test_func
+        self.text_img_gen = text_img_gen
+        self.num_display_words = num_display_words
 
-    # Two layers of bidirecitonal LSTMs
-    lstm_1 = LSTM(rnn_size, return_sequences=True, init='he_normal', name='lstm1')(inner)
-    lstm_1b = LSTM(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='lstm1_b')(inner)
-    lstm1_merged = merge([lstm_1, lstm_1b], mode='sum')     #see whether to sum or concat ??????????????????????????????????????????????????????????????
-    lstm_2 = LSTM(rnn_size, return_sequences=True, init='he_normal', name='lstm2')(lstm1_merged)
-    lstm_2b = LSTM(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='lstm2_b')(lstm1_merged)
+    # def show_edit_distance(self, num):
+    #     num_left = num
+    #     mean_norm_ed = 0.0
+    #     mean_ed = 0.0
+    #     while num_left > 0:
+    #         word_batch = next(self.text_img_gen)[0]
+    #         num_proc = min(word_batch['the_input'].shape[0], num_left)
+    #         decoded_res = decode_batch(self.test_func, word_batch['the_input'][0:num_proc])
+    #         for j in range(0, num_proc):
+    #             edit_dist = editdistance.eval(decoded_res[j], word_batch['source_str'][j])
+    #             mean_ed += float(edit_dist)
+    #             mean_norm_ed += float(edit_dist) / len(word_batch['source_str'][j])
+    #         num_left -= num_proc
+    #     mean_norm_ed = mean_norm_ed / num
+    #     mean_ed = mean_ed / num
+    #     print('\nOut of %d samples:  Mean edit distance: %.3f Mean normalized edit distance: %0.3f'
+    #           % (num, mean_ed, mean_norm_ed))
 
-    # transforms RNN output to character activations:
-    inner = Dense(num_class, init='he_normal',
-                  name='dense2')(merge([lstm_2, lstm_2b], mode='concat'))   # Giving dimension error ?????????????????????????????????????????
-    y_pred = Activation('softmax', name='softmax')(inner)
-    Model(input=[input_data], output=y_pred).summary()
+    def on_epoch_end(self, epoch):
+        # self.show_edit_distance(256)
+        word_batch = next(self.text_img_gen)[0]
+        res = decode_batch(self.test_func, word_batch['the_input'], word_batch['input_length'] )
+        print(np.mean(res == word_batch['source_str']))
 
-    labels = Input(name='the_labels', shape=[max_string_len], dtype='float32')
-    input_length = Input(name='input_length', shape=[1], dtype='int64')
-    label_length = Input(name='label_length', shape=[1], dtype='int64')
-    # Keras doesn't currently support loss funcs with extra parameters
-    # so CTC loss is implemented in a lambda layer
-    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
 
-    # clipnorm seems to speeds up convergence
-    sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+# def build_model():
+act = 'relu'
+input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
+                      activation=act, init='he_normal', name='conv1')(input_data)
+inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
+inner = Convolution2D(conv_num_filters, filter_size, filter_size, border_mode='same',
+                      activation=act, init='he_normal', name='conv2')(inner)
+inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
 
-    model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
+conv_to_rnn_dims = (time_length // (pool_size ** 2), (feature_length // (pool_size ** 2)) * conv_num_filters)
+inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
 
-    # the loss calc occurs elsewhere, so use a dummy lambda func for the loss
-    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+# cuts down input size going into RNN:
+inner = Dense(time_dense_size, activation=act, name='dense1')(inner)    # Giving dimension error ??????????????????????????????????????????????????
 
-    return model
+# Two layers of bidirecitonal LSTMs
+lstm_1 = LSTM(rnn_size, return_sequences=True, init='he_normal', name='lstm1')(inner)
+lstm_1b = LSTM(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='lstm1_b')(inner)
+lstm1_merged = merge([lstm_1, lstm_1b], mode='sum')     #see whether to sum or concat ??????????????????????????????????????????????????????????????
+lstm_2 = LSTM(rnn_size, return_sequences=True, init='he_normal', name='lstm2')(lstm1_merged)
+lstm_2b = LSTM(rnn_size, return_sequences=True, go_backwards=True, init='he_normal', name='lstm2_b')(lstm1_merged)
+
+# transforms RNN output to character activations:
+inner = Dense(num_class, init='he_normal',
+              name='dense2')(merge([lstm_2, lstm_2b], mode='concat'))   # Giving dimension error ?????????????????????????????????????????
+y_pred = Activation('softmax', name='softmax')(inner)
+Model(input=[input_data], output=y_pred).summary()
+
+labels = Input(name='the_labels', shape=[max_string_len], dtype='int32')
+input_length = Input(name='input_length', shape=[1], dtype='int64')
+label_length = Input(name='label_length', shape=[1], dtype='int64')
+# Keras doesn't currently support loss funcs with extra parameters
+# so CTC loss is implemented in a lambda layer
+loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+
+# clipnorm seems to speeds up convergence
+sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+
+model = Model(input=[input_data, labels, input_length, label_length], output=[loss_out])
+
+# the loss calc occurs elsewhere, so use a dummy lambda func for the loss
+model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=sgd)
+
+test_func = K.function([input_data],[y_pred])
+
+viz_cb = VizCallback(test_func, getdata())
+
+model.fit_generator(generator=getdata(), samples_per_epoch=40,
+                    nb_epoch=10, validation_data=getdata(), nb_val_samples=40,
+                    callbacks=[viz_cb])
+
+# build_model()
+
+
+
 
 # trainX, trainY, input_length, label_length, textX = getdata(time_length, input_time_length, max_string_len)
 
-model = build_model()
-model.fit_generator(generator=getdata(), samples_per_epoch=40, nb_epoch=10)
+
 
 # write('../data/s1.wav', 50000, data_audio[0])
